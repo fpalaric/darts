503721ed657fc5e0ba65ad8e002acede79827f65
fpalaric <36034881+fpalaric@users.noreply.github.com>
2018-03-08 21:31:27 +0100

    Add files via upload

--- js/301.js
+++ js/301.js
@@ -129,12 +129,12 @@ function button_register(){
 		var cell1 = table.rows[index].insertCell(table.rows[index].cells.length-1); // Insert a cell in second to last position
 		cell1.innerHTML=document.getElementById("score").value; // Insert the value 
 		var cell2=table.rows[index].cells[table.rows[index].cells.length-1];
-		var result=cell2.innerHTML-cell1.innerHTML; // Modify the 301 value (301-inserted score)
+		var result = cell2.innerHTML-cell1.innerHTML; // Modify the 301 value (301-inserted score)
 		if (result < 0)
 			{
-				cell2.innerHTML=cell2.innerHTML+cell1.innerHTML;
+				cell2.innerHTML=result+cell1.innerHTML;
 			}
-		else {cell2.innerHTML = results;}
+		else {cell2.innerHTML = result;}
 		if (cell2==0)
 			{
 				//TODO : display player i win and save the game
