132345ad5b95d414c95ea67ac30ef2235ea92093
fpalaric <36034881+fpalaric@users.noreply.github.com>
2018-03-08 20:54:25 +0100

    Add files via upload

--- js/301.js
+++ js/301.js
@@ -1,3 +1,4 @@
+// ------------------------
 let pnumber; // Number of players
 let aplayer=[]; // Array containing players names
 let save; // String used for saving scores
@@ -16,7 +17,7 @@ if(localStorage.getItem("temp_301")!="no_game"){
 	document.getElementById("playerNumber").style.display = "none";
 	document.getElementById("go").style.display = "none";
 	document.getElementById("enter_player").style.display = "none";
-	
+	document.getElementById("undo").style.display = "inline-block";
 	// Split the string temp_301 that contains the info of the previous game
 	// The string is formated this way : "temp_301/Player_1:2:299/Player_2:10:291/Player_3:301/Player_4:301"
 	let tab=localStorage.getItem("temp_301").split("/");
@@ -105,6 +106,7 @@ function enter_player(){
 	document.getElementById("table").style.display = "table";
 	document.getElementById("save").style.display = "inline-block";
 	document.getElementById("reset").style.display = "inline-block";
+	document.getElementById("undo").style.display = "inline-block";
 	// Insert the table with the players name and the 301 cell
 	let table = document.getElementById("table");
 	for (let i=0; i<pnumber; i++){
@@ -120,36 +122,39 @@ function enter_player(){
 // Called when the user register a score for a player
 function button_register(){
 	let value=document.getElementById("score").value;
-	if (value<=180&&value>0)
-	{
-	let select=document.getElementById("select");
-	let index=select.selectedIndex; 	//Get the index of the selected player (index = table row)
-	let table = document.getElementById("table");
-	var cell1 = table.rows[index].insertCell(table.rows[index].cells.length-1); // Insert a cell in second to last position
-	cell1.innerHTML=document.getElementById("score").value; // Insert the value 
-	var cell2=table.rows[index].cells[table.rows[index].cells.length-1];
-	cell2.innerHTML=cell2.innerHTML-cell1.innerHTML; // Modify the 301 value (301-inserted score)
-	if (cell2.innerHTML<0)
-		{
-			cell2.innerHTML=cell2.innerHTML+cell1.innerHTML; // We don't admit negatives values.
+	if(value <= 180 && value>0){
+		let select=document.getElementById("select");
+		let index=select.selectedIndex; 	//Get the index of the selected player (index = table row)
+		let table = document.getElementById("table");
+		var cell1 = table.rows[index].insertCell(table.rows[index].cells.length-1); // Insert a cell in second to last position
+		cell1.innerHTML=document.getElementById("score").value; // Insert the value 
+		var cell2=table.rows[index].cells[table.rows[index].cells.length-1];
+		cell2.innerHTML=cell2.innerHTML-cell1.innerHTML; // Modify the 301 value (301-inserted score)
+		// Construct the string used to save the data (see top of the code to see the format used)
+		save="temp_301";
+		for(let i=0; i<table.rows.length; i++){
+			save=save+"/"+table.rows[i].cells[0].innerHTML;
+			for (let j=1; j<table.rows[i].cells.length-1; j++){
+				save=save+":"+table.rows[i].cells[j].innerHTML;
+			}
 		}
-	if (cell2.innerHTML==0)
-		{
-			/* TODO link to a html page who says "player i won" + save of the game */
-		}
-	// Construct the string used to save the data (see top of the code to see the format used)
-	save="temp_301";
-	for(let i=0; i<table.rows.length; i++){
-		save=save+"/"+table.rows[i].cells[0].innerHTML;
-		for (let j=1; j<table.rows[i].cells.length-1; j++){
-			save=save+":"+table.rows[i].cells[j].innerHTML;
-		}
-	}
-	// Save the string into the localStorage
-	localStorage.setItem("temp_301", save);
+		// Save the string into the localStorage
+		localStorage.setItem("temp_301", save);
 	}
 }
 
+function button_undo(){
+	let table = document.getElementById("table");
+	let select=document.getElementById("select");
+	let index=select.selectedIndex; 	//Get the index of the selected player (index = table row)
+	console.log(table.rows[index].cells.length-2);
+	if(table.rows[index].cells.length-2>0 && table.rows[index].cells.length-2!=table.rows[index].cells.length-1){
+	var cell1=table.rows[index].cells[table.rows[index].cells.length-2];
+	var cell2=table.rows[index].cells[table.rows[index].cells.length-1];
+	cell2.innerHTML=parseInt(cell2.innerHTML)+parseInt(cell1.innerHTML);
+	table.rows[index].deleteCell(table.rows[index].cells.length-2);
+	}
+}
 
 function button_save(){
 	save="temp_301";
